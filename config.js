74fd3d7043a7bd2ac9225619871d6b64bb3aa870
shaohua <shaohua@users.noreply.github.com>
2026-07-04 16:24:36 -0700

    Update Morocco bracket result

--- config.js
+++ config.js
@@ -86,7 +86,7 @@ window.BRACKET_CONFIG = {
       "ma",  "ca",  "fr",  "py",   // Morocco, Canada, France, Paraguay advance
     ],
     // 8 R16 games.
-    r16: [null, null, null, null, null, null, null, "fr"],
+    r16: [null, null, null, null, null, null, "ma", "fr"],
     // 4 QF games.
     qf: [null, null, null, null],
     // 2 SF games.
